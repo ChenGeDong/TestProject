64290b4f6f7420646d00d85748c0ab26099de059
LDD <chenge126@126.com>
2021-11-16 08:43:23 +0800

    文件冲突合并提交

--- StaticContructorSample/UserPreferences.cs
+++ StaticContructorSample/UserPreferences.cs
@@ -13,10 +13,27 @@ namespace StaticContructorSample
             if(now.DayOfWeek == DayOfWeek.Saturday||now.DayOfWeek==DayOfWeek.Sunday)
             {
                 BackCorlor = Color.green;
+                BackCorlor = Color.green;
+                BackCorlor = Color.green;
+                BackCorlor = Color.green;
+                BackCorlor = Color.green;
+                BackCorlor = Color.green;
+                BackCorlor = Color.green;
+                BackCorlor = Color.green;
 
             }
             else
             {
+                BackCorlor = Color.red;
+                BackCorlor = Color.red;
+                BackCorlor = Color.red;
+
+                BackCorlor = Color.red;
+                BackCorlor = Color.red;
+                BackCorlor = Color.red;
+                BackCorlor = Color.red;
+                BackCorlor = Color.red;
+                BackCorlor = Color.red;
                 BackCorlor = Color.red;
             }
         }
