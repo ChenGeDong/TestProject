84c0ed66538cd1ab3b1a9a0802fae09693cdbf32
LDD <chenge126@126.com>
2021-11-19 09:19:11 +0800

    委托

---
+++ MulticcastDelegates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticcastDelegates
+{
+    /// <summary>
+    /// 多播委托的了解学习
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
